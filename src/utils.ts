67b3965a48f9e497f5c96536ba1a12bfb2381ad6
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-04-23 17:37:22 +0500

    Add utils.ts file for utility functions

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getSecret(secret: string | undefined) {
+  return secret || process.env.JWT_SECRET;
+}
