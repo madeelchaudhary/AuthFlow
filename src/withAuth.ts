455ac9cac1b72601b43dac76376b7db1ffcc96a3
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-04-26 12:43:10 +0500

    Refactor middleware to call handler when the route is a public route

--- src/withAuth.ts
+++ src/withAuth.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getAuthFlowConfig, setAuthFlowConfig } from "./constants";
-import { AuthenticationError, UnauthorizedError } from "./errors";
-import { decode } from "./jwt"; // Import verification and error handling
-import { AuthConfig, User } from "./types";
+import { NextRequest, NextResponse } from 'next/server'
+import { getAuthFlowConfig, setAuthFlowConfig } from './constants'
+import { AuthenticationError, UnauthorizedError } from './errors'
+import { decode } from './jwt' // Import verification and error handling
+import { AuthConfig, User } from './types'
 
 interface WithAuthHandler {
-  (req: NextRequest, res: NextResponse, user: User): Promise<void | any>;
+  (req: NextRequest, res: NextResponse, user?: User): Promise<void | any>
 }
 
 interface WithAuthPages {
@@ -19,7 +19,7 @@ interface WithAuthPages {
    *  public: ["/", "/about"]
    * }
    */
-  public?: string[];
+  public?: string[]
 
   /**
    * Override the default error page for authentication errors.
@@ -27,7 +27,7 @@ interface WithAuthPages {
    * @remarks
    * You can set this in the `AuthFlow` configuration.
    */
-  error?: string;
+  error?: string
 
   /**
    * Override the default sign-in page.
@@ -35,12 +35,12 @@ interface WithAuthPages {
    * @remarks
    * You can set this in the `AuthFlow` configuration.
    */
-  signin?: string;
+  signin?: string
 
   /**
    * Pages that if accessed by an authenticated user will redirect to the home page.
    */
-  unAuthonticated?: string[];
+  unAuthonticated?: string[]
 
   /**
    * Page to redirect to a user that is authenticated and tries to access a page in the `unAuthonticated` list or `signin' page.
@@ -49,12 +49,12 @@ interface WithAuthPages {
    * @remarks
    * This page should be a public page. If not, in certain cases, it will cause a redirect loop.
    */
-  home?: string;
+  home?: string
 }
 
 interface WithAuthOptions {
-  pages?: WithAuthPages;
-  config: AuthConfig;
+  pages?: WithAuthPages
+  config: AuthConfig
 }
 
 /**
@@ -65,108 +65,102 @@ interface WithAuthOptions {
  * @returns A middleware function that protects routes that require authentication.
  *
  */
-export default function withAuth(
-  { config, pages }: WithAuthOptions,
-  handler?: WithAuthHandler
-) {
-  setAuthFlowConfig(config); // Set the configuration for the authentication flow
+export default function withAuth({ config, pages }: WithAuthOptions, handler?: WithAuthHandler) {
+  setAuthFlowConfig(config) // Set the configuration for the authentication flow
   const {
     jwtSecret,
     adapter,
     pages: authFlowPages,
     session: { cookieName, strategy },
-  } = getAuthFlowConfig(); // Retrieve the configuration
+  } = getAuthFlowConfig() // Retrieve the configuration
 
   return async function (req: NextRequest, res: NextResponse) {
-    const unAuthonticatedPages = pages?.unAuthonticated || [];
-    const homePage = pages?.home || "/";
-    const errorPage = pages?.error || authFlowPages.error;
-    const signinPage = pages?.signin || authFlowPages.signin;
-    const publicPages = pages?.public || [];
+    const unAuthonticatedPages = pages?.unAuthonticated || []
+    const homePage = pages?.home || '/'
+    const errorPage = pages?.error || authFlowPages.error
+    const signinPage = pages?.signin || authFlowPages.signin
+    const publicPages = pages?.public || []
 
-    publicPages.push(errorPage);
-    unAuthonticatedPages.push(signinPage);
+    publicPages.push(errorPage)
+    unAuthonticatedPages.push(signinPage)
 
-    const path = req.nextUrl.pathname;
+    const path = req.nextUrl.pathname
 
-    if (
-      publicPages.includes(path) ||
-      publicPages.some((page) => path.startsWith(page))
-    ) {
-      return NextResponse.next(); // Call the next middleware if the user is on a public page
+    if (publicPages.includes(path) || publicPages.some((page) => path.startsWith(page))) {
+      if (handler) {
+        return handler(req, res) // Call the handler function if the user is on a public page
+      }
+      return NextResponse.next() // Call the next middleware if the user is on a public page
     }
 
     try {
-      const token = extractToken(req, cookieName); // Extract the token from the request
+      const token = extractToken(req, cookieName) // Extract the token from the request
 
       const payload = await decode({
         token,
         secret: jwtSecret,
-      }); // Verify the token
+      }) // Verify the token
 
-      let user: User | null = null;
+      let user: User | null = null
 
-      if (strategy === "session") {
-        const session = await adapter!.getUserFromSession?.(token); // Retrieve the user session
+      if (strategy === 'session') {
+        const session = await adapter!.getUserFromSession?.(token) // Retrieve the user session
 
         if (!session) {
-          throw new UnauthorizedError();
+          throw new UnauthorizedError()
         }
 
         if (session.session.expires < new Date()) {
-          throw new UnauthorizedError();
+          throw new UnauthorizedError()
         }
 
-        user = session.user; // Extract the user from the session
+        user = session.user // Extract the user from the session
       } else {
-        user = await adapter!.getUserByIdentifier((payload as any)._identifier); // Retrieve the user from the database
+        user = await adapter!.getUserByIdentifier((payload as any)._identifier) // Retrieve the user from the database
       }
 
       if (!user) {
-        throw new UnauthorizedError();
+        throw new UnauthorizedError()
       }
 
-      if (
-        unAuthonticatedPages.includes(path) ||
-        unAuthonticatedPages.some((page) => path.startsWith(page))
-      ) {
-        return NextResponse.redirect(new URL(homePage, req.url)); // Redirect to the home page if the user is on an unauthenticated page
+      if (unAuthonticatedPages.includes(path) || unAuthonticatedPages.some((page) => path.startsWith(page))) {
+        return NextResponse.redirect(new URL(homePage, req.url)) // Redirect to the home page if the user is on an unauthenticated page
       }
 
       if (!handler) {
-        return NextResponse.next(); // Call the next middleware if no handler is provided
+        return NextResponse.next() // Call the next middleware if no handler is provided
       }
 
-      return handler(req, res, user); // Call the protected handler function with the user object
+      return handler(req, res, user) // Call the protected handler function with the user object
     } catch (error) {
       if (
         path.startsWith(authFlowPages.signup) ||
         unAuthonticatedPages.includes(path) ||
         unAuthonticatedPages.some((page) => path.startsWith(page))
       ) {
-        return NextResponse.next(); // Call the next middleware if the user is on an auth page
+        return NextResponse.next() // Call the next middleware if the user is on an auth page
       }
 
       if (error instanceof AuthenticationError) {
-        return NextResponse.redirect(new URL(signinPage, req.url)); // Redirect to the sign-in page
+        return NextResponse.redirect(new URL(signinPage, req.url)) // Redirect to the sign-in page
       }
       // Handle other errors (e.g., database errors)
-      console.error(error, "Error in withAuth middleware"); // Log the error
-      return NextResponse.redirect(new URL(errorPage, req.url)); // Redirect to the error page
+      console.error(error, 'Error in withAuth middleware') // Log the error
+      return NextResponse.redirect(new URL(errorPage, req.url)) // Redirect to the error page
     }
-  };
+  }
 }
 
 const extractToken = (req: NextRequest, cookieName: string) => {
-  let token = req.cookies.get(cookieName)?.value; // Extract token from cookie
+  let token = req.cookies.get(cookieName)?.value // Extract token from cookie
 
   if (!token) {
-    token = req.headers.get("Authorization")?.split(" ")[1]; // Extract token from authorization header
+    token = req.headers.get('Authorization')?.split(' ')[1] // Extract token from authorization header
   }
 
   if (!token) {
-    throw new UnauthorizedError();
+    throw new UnauthorizedError()
   }
 
-  return token;
-};
+  return token
+}
--- src/withAuthEdge.ts
+++ src/withAuthEdge.ts
@@ -1,13 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getAuthFlowConfig, setAuthFlowConfig } from "./constants";
-import { AuthenticationError, UnauthorizedError } from "./errors";
-import { decode } from "./jwt"; // Import verification and error handling
-import { AuthConfig } from "./types";
+import { NextRequest, NextResponse } from 'next/server'
+import { getAuthFlowConfig, setAuthFlowConfig } from './constants'
+import { AuthenticationError, UnauthorizedError } from './errors'
+import { decode } from './jwt' // Import verification and error handling
+import { AuthConfig } from './types'
 
 interface WithAuthHandler {
-  (req: NextRequest, res: NextResponse, jwt: Record<string, any>): Promise<
-    void | any
-  >;
+  (req: NextRequest, res: NextResponse, jwt?: Record<string, any>): Promise<void | any>
 }
 
 interface WithAuthPages {
@@ -21,7 +19,7 @@ interface WithAuthPages {
    *  public: ["/", "/about"]
    * }
    */
-  public?: string[];
+  public?: string[]
 
   /**
    * Override the default error page for authentication errors.
@@ -29,7 +27,7 @@ interface WithAuthPages {
    * @remarks
    * You can set this in the `AuthFlow` configuration.
    */
-  error?: string;
+  error?: string
 
   /**
    * Override the default sign-in page.
@@ -37,12 +35,12 @@ interface WithAuthPages {
    * @remarks
    * You can set this in the `AuthFlow` configuration.
    */
-  signin?: string;
+  signin?: string
 
   /**
    * Pages that if accessed by an authenticated user will redirect to the home page.
    */
-  unAuthonticated?: string[];
+  unAuthonticated?: string[]
 
   /**
    * Page to redirect to a user that is authenticated and tries to access a page in the `unAuthonticated` list or `signin' page.
@@ -51,12 +49,12 @@ interface WithAuthPages {
    * @remarks
    * This page should be a public page. If not, in certain cases, it will cause a redirect loop.
    */
-  home?: string;
+  home?: string
 }
 
 interface WithAuthOptions {
-  pages?: WithAuthPages;
-  config: AuthConfig;
+  pages?: WithAuthPages
+  config: AuthConfig
 }
 
 /**
@@ -67,84 +65,78 @@ interface WithAuthOptions {
  * @returns A middleware function that protects routes that require authentication.
  *
  */
-export default function withAuthEdge(
-  { config, pages }: WithAuthOptions,
-  handler?: WithAuthHandler
-) {
-  setAuthFlowConfig(config); // Set the configuration for the authentication flow
+export default function withAuthEdge({ config, pages }: WithAuthOptions, handler?: WithAuthHandler) {
+  setAuthFlowConfig(config) // Set the configuration for the authentication flow
   const {
     jwtSecret,
     pages: authFlowPages,
     session: { cookieName },
-  } = getAuthFlowConfig(); // Retrieve the configuration
+  } = getAuthFlowConfig() // Retrieve the configuration
 
   return async function (req: NextRequest, res: NextResponse) {
-    const unAuthonticatedPages = pages?.unAuthonticated || [];
-    const homePage = pages?.home || "/";
-    const errorPage = pages?.error || authFlowPages.error;
-    const signinPage = pages?.signin || authFlowPages.signin;
-    const publicPages = pages?.public || [];
+    const unAuthonticatedPages = pages?.unAuthonticated || []
+    const homePage = pages?.home || '/'
+    const errorPage = pages?.error || authFlowPages.error
+    const signinPage = pages?.signin || authFlowPages.signin
+    const publicPages = pages?.public || []
 
-    publicPages.push(errorPage);
-    unAuthonticatedPages.push(signinPage);
+    publicPages.push(errorPage)
+    unAuthonticatedPages.push(signinPage)
 
-    const path = req.nextUrl.pathname;
-    if (
-      publicPages.includes(path) ||
-      publicPages.some((page) => path.startsWith(page))
-    ) {
-      return NextResponse.next(); // Call the next middleware if the user is on a public page
+    const path = req.nextUrl.pathname
+    if (publicPages.includes(path) || publicPages.some((page) => path.startsWith(page))) {
+      if (handler) {
+        return handler(req, res) // Call the handler function if the user is on a public page
+      }
+      return NextResponse.next() // Call the next middleware if the user is on a public page
     }
 
     try {
-      const token = extractToken(req, cookieName); // Extract the token from the request
+      const token = extractToken(req, cookieName) // Extract the token from the request
 
       const payload = await decode({
         token,
         secret: jwtSecret,
-      }); // Verify the token
+      }) // Verify the token
 
-      if (
-        unAuthonticatedPages.includes(path) ||
-        unAuthonticatedPages.some((page) => path.startsWith(page))
-      ) {
-        return NextResponse.redirect(new URL(homePage, req.url)); // Redirect to the home page if the user is on an unauthenticated page
+      if (unAuthonticatedPages.includes(path) || unAuthonticatedPages.some((page) => path.startsWith(page))) {
+        return NextResponse.redirect(new URL(homePage, req.url)) // Redirect to the home page if the user is on an unauthenticated page
       }
 
       if (!handler) {
-        return NextResponse.next(); // Call the next middleware if no handler is provided
+        return NextResponse.next() // Call the next middleware if no handler is provided
       }
 
-      return handler(req, res, payload); // Call the protected handler function with the user object
+      return handler(req, res, payload) // Call the protected handler function with the user object
     } catch (error) {
       if (
         path.startsWith(authFlowPages.signup) ||
         unAuthonticatedPages.includes(path) ||
         unAuthonticatedPages.some((page) => path.startsWith(page))
       ) {
-        return NextResponse.next(); // Call the next middleware if the user is on an auth page
+        return NextResponse.next() // Call the next middleware if the user is on an auth page
       }
 
       if (error instanceof AuthenticationError) {
-        return NextResponse.redirect(new URL(signinPage, req.url)); // Redirect to the sign-in page
+        return NextResponse.redirect(new URL(signinPage, req.url)) // Redirect to the sign-in page
       }
       // Handle other errors (e.g., database errors)
-      console.error(error, "Error in withAuth middleware"); // Log the error
-      return NextResponse.redirect(new URL(errorPage, req.url)); // Redirect to the error page
+      console.error(error, 'Error in withAuth middleware') // Log the error
+      return NextResponse.redirect(new URL(errorPage, req.url)) // Redirect to the error page
     }
-  };
+  }
 }
 
 const extractToken = (req: NextRequest, cookieName: string) => {
-  let token = req.cookies.get(cookieName)?.value; // Extract token from cookie
+  let token = req.cookies.get(cookieName)?.value // Extract token from cookie
 
   if (!token) {
-    token = req.headers.get("Authorization")?.split(" ")[1]; // Extract token from authorization header
+    token = req.headers.get('Authorization')?.split(' ')[1] // Extract token from authorization header
   }
 
   if (!token) {
-    throw new UnauthorizedError();
+    throw new UnauthorizedError()
   }
 
-  return token;
-};
+  return token
+}
